789b6705620e3bf4238f86e30d10ffc35b2f619b
Owen <oaw6@georgetown.edu>
2019-02-15 12:46:08 -0500

    Introduce the program

--- monthly_sales.py
+++ monthly_sales.py
@@ -5,9 +5,27 @@ import os
 
 # TODO: write some Python code here to produce the desired functionality...
 
+#Introduces the program
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("Welcome to the Executive Dashboard Program!")
+print("This program will allow you to quickly summarize")
+print("the sales details of the month of your choosing.")
+print("Below is a list of .csv files which correspond")
+print("to months for which we have data.")
+print("--------------------------------------------")
+print("--------------------------------------------")
+
+#Prints the list of available csv files in the "data" sub-directory of the current repository
 current_directory = os.getcwd()
 csv_file_list = os.listdir(current_directory + "/data")
 print(csv_file_list)
+
+#Asks which month the user wants to explore
+print("--------------------------------------------")
+print("--------------------------------------------")
+print("Please type in the name of the file for the ")
+print("month you want to explore:")
 
 print("-----------------------")
 print("MONTH: March 2018")
